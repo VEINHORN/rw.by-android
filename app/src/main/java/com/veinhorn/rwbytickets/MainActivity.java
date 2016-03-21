fed03d094bfb7f1f0b82214fa52d03649ae3c3f7
VEINHORN <b.korogvich@gmail.com>
2016-03-21 20:42:48 +0300

    added http client libs, added simple sign in url fetcher

--- app/src/main/java/com/veinhorn/rwbytickets/MainActivity.java
+++ app/src/main/java/com/veinhorn/rwbytickets/MainActivity.java
@@ -1,15 +1,37 @@
 package com.veinhorn.rwbytickets;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.internal.framed.Header;
+
 public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient httpClient;
+    private static final String BASE_URL = "https://poezd.rw.by";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +48,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        httpClient = new OkHttpClient();
+
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                //Response response = null;
+                try {
+                    // Get proper sign in form url
+                    Request request = new Request.Builder()
+                            .url("https://poezd.rw.by/wps/portal/home/login_main")
+                            .build();
+                    Response response = httpClient.newCall(request).execute();
+
+                    Document document = Jsoup.parse(response.body().string());
+                    Element element = document.getElementById("login");
+
+                    String actionUrl = element.attr("action");
+
+                    ///// Sign in
+                    String signInUrl = BASE_URL + actionUrl; // compose login url for auth
+
+                    // Create body with POST parameters
+                    RequestBody formBody = new FormBody.Builder()
+                            .add("login", "")
+                            .add("password", "")
+                            .build();
+
+                    Request signInRequest = new Request.Builder()
+                            .url(signInUrl)
+                            .post(formBody)
+                            .build();
+
+                    Response signInResponse = httpClient.newCall(signInRequest).execute();
+
+                    Integer test = 3450345;
+                    /////
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return "";
+            }
+        }.execute();
     }
 
     @Override
