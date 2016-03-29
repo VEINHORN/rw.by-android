4302cb3cb8eb395bdab78ac14fd204c3b8e1e7e0
VEINHORN <b.korogvich@gmail.com>
2016-03-30 00:13:13 +0300

    replaced some code with butterknife approach

--- app/src/main/java/com/veinhorn/rwbytickets/MainActivity.java
+++ app/src/main/java/com/veinhorn/rwbytickets/MainActivity.java
@@ -7,15 +7,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.veinhorn.rwbytickets.search.StationAutoCompleteAdapter;
 import com.veinhorn.rwbytickets.search.rest.model.Station;
 import com.veinhorn.rwbytickets.search.view.DelayAutoCompleteTextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final int DEFAULT_THRESHOLD = 2;
@@ -23,17 +23,17 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.toolbar) protected Toolbar toolbar;
     @Bind(R.id.continueButton) protected Button continueButton;
 
-    private DelayAutoCompleteTextView fromStationView;
-    private DelayAutoCompleteTextView toStationView; // butterknife cannot bind this ??
+    @Bind(R.id.fromStationView) protected DelayAutoCompleteTextView fromStationView;
+    @Bind(R.id.toStationView) protected DelayAutoCompleteTextView toStationView; // butterknife cannot bind this ??
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
 
-        fromStationView = (DelayAutoCompleteTextView) findViewById(R.id.fromStationView);
         fromStationView.setThreshold(DEFAULT_THRESHOLD);
         fromStationView.setAdapter(new StationAutoCompleteAdapter(this));
         fromStationView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        toStationView = (DelayAutoCompleteTextView) findViewById(R.id.toStationView);
         toStationView.setThreshold(DEFAULT_THRESHOLD);
         toStationView.setAdapter(new StationAutoCompleteAdapter(this));
         toStationView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -54,12 +53,10 @@ public class MainActivity extends AppCompatActivity {
                 toStationView.setText(station.getName());
             }
         });
+    }
 
-        continueButton.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                new TicketsLoader(MainActivity.this).execute();
-            }
-        });
+    @OnClick(R.id.continueButton) public void continuePurchase() {
+        new TicketsLoader(this).execute();
     }
 
     @Override
