f40f663d9cbe622e4ec2c187622ef310ef96858d
VEINHORN <b.korogvich@gmail.com>
2016-03-30 23:56:29 +0300

    added action logic structure (every step in purchase is represented as an action)

---
+++ app/src/main/java/com/veinhorn/rwbytickets/search/action/Action.java
@@ -0,0 +1,10 @@
+package com.veinhorn.rwbytickets.search.action;
+
+import okhttp3.Response;
+
+/**
+ * Created by veinhorn on 30.3.16.
+ */
+public interface Action {
+    Response doAction();
+}
---
+++ app/src/main/java/com/veinhorn/rwbytickets/search/action/SignInAction.java
@@ -0,0 +1,14 @@
+package com.veinhorn.rwbytickets.search.action;
+
+import okhttp3.Response;
+
+/**
+ * Created by veinhorn on 30.3.16.
+ */
+public class SignInAction implements Action {
+
+    @Override
+    public Response doAction() {
+        
+    }
+}
