0864fdada24eb4c1b5a09c7fa5579ea356b09fd1
VEINHORN <b.korogvich@gmail.com>
2016-05-04 23:30:00 +0300

    added several improvements to AuthLoader

--- app/src/main/java/com/veinhorn/rwbytickets/auth/AuthLoader.java
+++ app/src/main/java/com/veinhorn/rwbytickets/auth/AuthLoader.java
@@ -1,7 +1,9 @@
 package com.veinhorn.rwbytickets.auth;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import com.veinhorn.rwbytickets.action.SignInAction;
 import com.veinhorn.rwbytickets.auth.creds.DefaultCreds;
@@ -16,28 +18,40 @@ import okhttp3.Response;
  */
 public class AuthLoader extends AsyncTask<String, Void, Boolean> {
 
-    private Context context;
-    private Dialog dialog;
+    private Activity activity;
     private ICreds credentials;
 
-    public AuthLoader(Context context, ICreds credentials) {
-        this.context = context;
-        dialog = DialogFactory.newInstance();
-        this.credentials = credentials;
+    public AuthLoader(Activity activity, ICreds credentials) {
+        this.activity = activity;
+        if (credentials.getLogin().equals("") || credentials.getPassword().equals("")) {
+            this.credentials = new DefaultCreds(activity);
+        } else {
+            this.credentials = credentials;
+        }
     }
 
     @Override protected Boolean doInBackground(String... params) {
         try {
-            Response signInResponse = new SignInAction(context, credentials)
-                    .doAction(dialog);
-            Integer test = 234234;
+            Response signInResponse = new SignInAction(activity, credentials)
+                    .doAction(DialogFactory.newInstance());
+            return isSignInSuccessful(signInResponse);
         } catch (Exception e) {
-            Integer test2 = 23423;
+            Integer test = 23423;
         }
-        return true;
+        return false;
     }
 
-    protected void onPostExecute(Boolean result) {
+    private boolean isSignInSuccessful(Response res) {
+        return res.code() == 200 &&
+                "https://poezd.rw.by/wps/myportal/home/rp/buyTicket".equals(res.request().url());
+    }
 
+    protected void onPostExecute(Boolean isSignInSuccessful) {
+        if (isSignInSuccessful) {
+            Integer yo = 12345;
+            activity.finish();
+        } else {
+            Toast.makeText(activity, "Cannot sign in. Try again", Toast.LENGTH_SHORT).show();
+        }
     }
 }
